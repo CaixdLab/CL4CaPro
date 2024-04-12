b45d231fbaf39906afb47b3367e86efdfe9e579e
Anchen Sun <anchensun95@gmail.com>
2024-04-12 10:32:41 -0400

    Initialize the package for Contrastive Learning for Predicting Cancer Prognosis Using Gene Expression Values

--- Auto_Train_GPU.py
+++ Auto_Train_GPU.py
@@ -5,7 +5,20 @@ seed_list = [10, 74, 341, 925, 1036, 1555, 1777, 2030, 2060, 2090, 2200,
              2222, 2268, 2289, 2341, 2741, 2765, 2782, 2857, 2864, 2918,
              2937, 2948, 2960, 2968, 3005, 3008, 3154, 3199, 3212, 3388,
              3455, 3466, 3611, 3679, 3927, 4000, 4013, 4416, 4520]
-def run_training(get_seed, device):
+
+def parse_option():
+    parser = argparse.ArgumentParser('argument for generate auto validation results')
+
+    parser.add_argument('--task', type=str, default='Classifier', choices=['Classifier', 'Cox'],
+                        help='task (Classifier/Cox)')
+    parser.add_argument('--cancer_group', type=str, default='SM',
+                        help='cancer group (SM/NGT/MSE/CCPRC/HC/GG/DS/LC)')
+
+    opt = parser.parse_args()
+
+    return opt
+
+def run_training(get_seed, device, cancer_group, task):
     # Fine-Tune 2 + 1 layers
     dim_1_list = [5196]
     dim_2_list = [2048]
@@ -14,8 +27,8 @@ def run_training(get_seed, device):
     seed = get_seed
     train_epoch = 5000
     round_num = 1
-    cancer_group = 'BRCA'
-    task = 'Risk' # use 'Risk" to train a classifier, use 'WholeTimeSeq' to train Cox models
+    #cancer_group = 'BRCA'
+    #task = 'Risk' # use 'Risk" to train a classifier, use 'WholeTimeSeq' to train Cox models
 
     if cancer_group == 'SM' or cancer_group == 'BLCA' or cancer_group == 'CESC' or cancer_group == 'HNSC' or cancer_group == 'LUSC':
         input_dim = 16008
@@ -101,31 +114,37 @@ def run_training(get_seed, device):
                             print('Execute following:', order_line)
                             os.system(order_line)
     return 'Finish seed ' + str(get_seed) + ' on device ' + str(device)
-# specify the number of threads to use
-num_threads = 4
 
-# divide the seeds into two equal parts for each device
-seeds_per_device = 20
+def main():
+    opt_get = parse_option()
+    task_get = opt_get.task
+    cancer_get = opt_get.cancer_group
 
-# create a list of thread objects
-threads = []
+    # specify the number of threads to use
+    num_threads = 4
 
-# create threads for device 0
-with concurrent.futures.ProcessPoolExecutor(num_threads) as executor:
-    for i in range(5):
-        start = i * num_threads
-        end = (i + 1) * num_threads
+    # divide the seeds into two equal parts for each device
+    seeds_per_device = 20
 
-        # create a new thread and add it to the list
-        for result in executor.map(run_training, seed_list[start:end], [0] * num_threads):
-            print(result)
+    # create a list of thread objects
+    threads = []
+
+    # create threads for device 0
+    with concurrent.futures.ProcessPoolExecutor(num_threads) as executor:
+        for i in range(5):
+            start = i * num_threads
+            end = (i + 1) * num_threads
+
+            # create a new thread and add it to the list
+            for result in executor.map(run_training, seed_list[start:end], [0] * num_threads, [cancer_get] * num_threads, [task_get] * num_threads):
+                print(result)
 
 
-with concurrent.futures.ProcessPoolExecutor(num_threads) as executor:
-    for i in range(5):
-        start = i * num_threads + seeds_per_device
-        end = (i + 1) * num_threads + seeds_per_device
+    with concurrent.futures.ProcessPoolExecutor(num_threads) as executor:
+        for i in range(5):
+            start = i * num_threads + seeds_per_device
+            end = (i + 1) * num_threads + seeds_per_device
 
-        # create a new thread and add it to the list
-        for result in executor.map(run_training, seed_list[start:end], [1] * num_threads):
-            print(result)
+            # create a new thread and add it to the list
+            for result in executor.map(run_training, seed_list[start:end], [1] * num_threads, [cancer_get] * num_threads, [task_get] * num_threads):
+                print(result)
--- Auto_Vali.py
+++ Auto_Vali.py
@@ -1,21 +1,52 @@
 import os
 import concurrent.futures
+import argparse
 
 seed_list = [10, 74, 341, 925, 1036, 1555, 1777, 2030, 2060, 2090, 2200,
              2222, 2268, 2289, 2341, 2741, 2765, 2782, 2857, 2864, 2918,
              2937, 2948, 2960, 2968, 3005, 3008, 3154, 3199, 3212, 3388,
              3455, 3466, 3611, 3679, 3927, 4000, 4013, 4416, 4520]
-# specify the number of threads to use
-# cancer_group_list = ["BLCA", "BRCA", "CESC", "COAD", "HNSC", "KIRC", "KIRP", "LGG", "LIHC", "LUAD", "LUSC", "OV", "PRAD", "SARC", "SKCM", "STAD", "THCA", "UCEC"]
-cancer_group_get = 'BLCA'
-num_threads = 10
-task = 'Classifier' # 'Cox'
-for seed in seed_list:
-    if task == 'Classifier':
-        command = 'python Classifier_method.py --seed {} --cancer_group {} --core {} > ClassifierLog/{}.log &'.format(
-            seed, cancer_group_get, num_threads)
-    elif task == 'Cox':
-        command = 'python Cox_method.py --seed {} --cancer_group {} --core {} > CoxLog/{}.log &'.format(
-            seed, cancer_group_get, num_threads)
-    os.system(command)
-    print('Finish ' + cancer_group_get)
+
+def parse_option():
+    parser = argparse.ArgumentParser('argument for generate auto validation results')
+
+    parser.add_argument('--num_threads', type=int, default=10,
+                        help='number of threads')
+    parser.add_argument('--task', type=str, default='Classifier', choices=['Classifier', 'Cox'],
+                        help='task (Classifier/Cox)')
+    parser.add_argument('--cancer_group', type=str, default='SM',
+                        help='cancer group (SM/NGT/MSE/CCPRC/HC/GG/DS/LC)')
+    parser.add_argument('--seed', type=int, default=0,
+                        help='random seed')
+
+    opt = parser.parse_args()
+
+    return opt
+
+def main():
+    opt_get = parse_option()
+    # specify the number of threads to use
+    # cancer_group_list = ["BLCA", "BRCA", "CESC", "COAD", "HNSC", "KIRC", "KIRP", "LGG", "LIHC", "LUAD", "LUSC", "OV", "PRAD", "SARC", "SKCM", "STAD", "THCA", "UCEC"]
+    cancer_group_get = opt_get.cancer_group
+    num_threads = opt_get.seed
+    task = opt_get.task
+    seed_get = opt_get.seed
+    if seed_get != 0:
+        for seed in seed_list:
+            if task == 'Classifier':
+                command = 'python Classifier_method.py --seed {} --cancer_group {} --core {} > ClassifierLog/{}.log &'.format(
+                    seed, cancer_group_get, num_threads)
+            elif task == 'Cox':
+                command = 'python Cox_method.py --seed {} --cancer_group {} --core {} > CoxLog/{}.log &'.format(
+                    seed, cancer_group_get, num_threads)
+            os.system(command)
+            print('Finish ' + cancer_group_get)
+    else:
+        if task == 'Classifier':
+            command = 'python Classifier_method.py --seed {} --cancer_group {} --core {} > ClassifierLog/{}.log &'.format(
+                seed_get, cancer_group_get, num_threads)
+        elif task == 'Cox':
+            command = 'python Cox_method.py --seed {} --cancer_group {} --core {} > CoxLog/{}.log &'.format(
+                seed_get, cancer_group_get, num_threads)
+        os.system(command)
+        print('Finish ' + cancer_group_get)
